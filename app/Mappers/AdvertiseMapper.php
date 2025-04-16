9c4ecd12372b53b3968366851a2f9b210d125413
esmaeel <esmaeel.ghasemi.org@gmail.com>
2025-04-16 17:53:29 +0330

    Add getUserAdvertisesByTag to advertise mapper

--- app/Mappers/AdvertiseMapper.php
+++ app/Mappers/AdvertiseMapper.php
@@ -31,4 +31,18 @@ class AdvertiseMapper extends Mapper
             'user.username' => $user->username,
         ], true);
     }
+
+    /**
+     * Get user advertises by tag
+     * @param User $user
+     * @param string $tag
+     * @return array
+     */
+    public function getUserAdvertisesByTag(User $user, string $tag): array
+    {
+        return $this->dataset->select([
+            'user.username' => $user->username,
+            'tag' => $tag,
+        ], true);
+    }
 }
