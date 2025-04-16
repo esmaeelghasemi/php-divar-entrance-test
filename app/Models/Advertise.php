5f98e161bf38985c7a8c9a77e140721877dcdb50
esmaeel <esmaeel.ghasemi.org@gmail.com>
2025-04-16 17:53:13 +0330

    Add tag to Advertise Model

--- app/Models/Advertise.php
+++ app/Models/Advertise.php
@@ -9,11 +9,13 @@ class Advertise extends Model
     /**
      * @param User $user
      * @param string $title
+     * @param string|null $tag
      * @param string|null $publishedAt
      */
     public function __construct(
         public User $user,
         public string $title,
+        public ?string $tag = null,
         public ?string $publishedAt = null,
     ){
 
