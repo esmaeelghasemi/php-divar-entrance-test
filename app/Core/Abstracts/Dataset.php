b37639100f52a83f45943bdd4b0a0c5b4e4c8acd
esmaeel <esmaeel.ghasemi.org@gmail.com>
2025-04-16 13:43:15 +0330

    Add multiple flag to be able to select multi data , get item field value by key method

--- app/Core/Abstracts/Dataset.php
+++ app/Core/Abstracts/Dataset.php
@@ -2,8 +2,6 @@
 
 namespace App\Core\Abstracts;
 
-use App\Core\Traits\ShouldSingleton;
-
 abstract class Dataset
 {
     /**
@@ -40,15 +38,38 @@ abstract class Dataset
     /**
      * Select and return a value from data
      * @param array $data
-     * @return Model|null
+     * @param bool $multiple
+     * @return array|Model|null
      */
-    public function select(array $data): ?Model
+    public function select(array $data, bool $multiple = false): array|Model|null
     {
-        return $this->doSelect($data);
+        return $this->doSelect($data, $multiple);
     }
 
+
     abstract protected function doAdd(Model $model): bool;
+
     abstract protected function doRemove(Model $model): bool;
-    abstract protected function doSelect(array $data): ?Model;
+
+    abstract protected function doSelect(array $data, bool $isMultiple = false): array|Model|null;
+
     abstract protected function doUpdate(Model $model, array $data): ?Model;
+
+    /**
+     * Prepare to get item field value by key
+     * @param Model $model
+     * @param string $key
+     * @return mixed|null
+     */
+    protected function getItemFieldValueByKey(Model $model, string $key): mixed
+    {
+        $val = null;
+        $parseKey = explode('.', $key);
+        foreach ($parseKey as $part) {
+
+            $val = !is_null($val) ? $val->$part : $model->$part;
+        }
+
+        return $val;
+    }
 }
